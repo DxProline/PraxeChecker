20faaca7cc711aaf4a519a61faa9bd97c2e65856
DxProline <jendad185@gmail.com>
2023-04-22 14:55:02 +0200

    3nd commit

--- src/main/java/cz/tmobile/ibmd/checker/Main.java
+++ src/main/java/cz/tmobile/ibmd/checker/Main.java
@@ -2,6 +2,6 @@ package cz.tmobile.ibmd.checker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ahoj");
+        System.out.println("Ahoj 0101001010001110100");
     }
 }
