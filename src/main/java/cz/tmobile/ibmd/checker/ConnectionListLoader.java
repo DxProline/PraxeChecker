b02116ce9f2baf6d59e4e69dc062979116030ba4
DxProline <jendad185@gmail.com>
2023-05-02 21:51:58 +0200

    BufferedReader

--- src/main/java/cz/tmobile/ibmd/checker/ConnectionListLoader.java
+++ src/main/java/cz/tmobile/ibmd/checker/ConnectionListLoader.java
@@ -2,6 +2,6 @@ package cz.tmobile.ibmd.checker;
 
 public class ConnectionListLoader {
     public ConnectionList load(String filename){
-        return null;
+        BufferedReader reader;
     }
 }
