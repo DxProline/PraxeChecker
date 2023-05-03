0b9fc5cbf29d58fda22a3d1a9b833db1bd3557b7
DxProline <jendad185@gmail.com>
2023-05-03 20:57:28 +0200

    BufferedReader and print

--- src/main/java/cz/tmobile/ibmd/checker/Main.java
+++ src/main/java/cz/tmobile/ibmd/checker/Main.java
@@ -1,9 +1,11 @@
 package cz.tmobile.ibmd.checker;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ConnectionListLoader connectionListLoader = new ConnectionListLoader();
-        connectionListLoader.load("xxx");
+        connectionListLoader.load("C:\\Users\\jiric\\Documents\\ahoj.txt");
 
     }
 }
