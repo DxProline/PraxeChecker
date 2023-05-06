412aa5742e5d1d555b7deafce9c95e8d2c169554
DxProline <jendad185@gmail.com>
2023-05-06 18:10:14 +0200

    ProcessListLoader Hotov.

--- src/main/java/cz/tmobile/ibmd/checker/Process.java
+++ src/main/java/cz/tmobile/ibmd/checker/Process.java
@@ -7,4 +7,34 @@ public class Process {
     private String processName;
     private String host;
     private Integer port;
+
+    public Process(String processName, String host, Integer port) {
+        this.processName = processName;
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
 }
--- src/main/java/cz/tmobile/ibmd/checker/ProcessListLoader.java
+++ src/main/java/cz/tmobile/ibmd/checker/ProcessListLoader.java
@@ -1,8 +1,28 @@
 package cz.tmobile.ibmd.checker;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ProcessListLoader {
-    public ProcessList load(String filename){
-        //dů dodělat to co je v connectionListLoadru
-        return null;
+    // Funkce která načítá soubor pomocí BufferedReadru
+    public ProcessList load(String filename) throws IOException {
+        BufferedReader reader;
+        ProcessList processList = new ProcessList();
+        // Vytvořen objekt BufferedReader
+        reader = new BufferedReader(new FileReader(filename));
+        ProcessList ProcessList = new ProcessList();
+        String line = reader.readLine();
+        //Rozdělí řádek načtený ze souboru na sloupce oddělené středníkem
+        String[] output = line.split(";");
+
+        //Null = dokud nedosáhne konec souboru
+        while (line != null) {
+            int port = Integer.parseInt(output[6]);
+            ProcessList.getProcesses().add(new Process(output[0], output[1], port));
+            // přečte další řádek
+            line = reader.readLine();
+    }
+        return processList;
     }
 }
