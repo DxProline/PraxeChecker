e2d2aeebfb80374fde0967d55608d0eb1b6e1662
DxProline <jendad185@gmail.com>
2023-05-06 16:24:35 +0200

    Class ConnectionListLoader Complete for homework do ProcessListLoader.

--- src/main/java/cz/tmobile/ibmd/checker/Process.java
+++ src/main/java/cz/tmobile/ibmd/checker/Process.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Process {
     private String processName;
-    private ArrayList <String> processIPAddresses;
-    private List<Integer> processPorts;
+    private String host;
+    private Integer port;
 }
