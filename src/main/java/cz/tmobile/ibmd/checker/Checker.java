a0a7ce0ed03f26ee2db83b50b85e101eca6254c6
DxProline <jendad185@gmail.com>
2023-05-11 19:31:10 +0200

    Diagram Process done.

--- src/main/java/cz/tmobile/ibmd/checker/Checker.java
+++ src/main/java/cz/tmobile/ibmd/checker/Checker.java
@@ -9,6 +9,9 @@ public class Checker {
                 return true;
             }
         }
+        //TODO Porovnat případ kdy destination server obsahuje více IP adres odělených znakem |
+        //TODO případy kdy destination server obsahuje celý rozsah IP adres např.: 10.238.0.20 - 10.238.0.30
+        //TODO případy kdy: 10.238.0.40-50
             //false = Tahle connectiona se nepoužívá v tomhle procesu
         return false;
     }
@@ -30,6 +33,24 @@ public class Checker {
             //Nenašel se ani jeden process který používá tuto connectionu
             if (countprocess == 0){
                 result.getRemovedServers().add(connection);
+            }
+        }
+        for (Process process : processList.getProcesses()) {
+            //čítač Connectionů které používžá daný process
+            Integer countconnection = 0;
+            for (Connection connection : connectionList.getConnections()) {
+                // Používá danný process danou Connection == výsledek porovnání Ano
+                if (compare(process, connection) == true){
+
+
+                    //zvedne se jen tehdy o 1 když daný proces používá danou conectionu
+                    countconnection++;
+                }
+            }
+            //Nenašel se ani jeden connection který používá daný process
+            if (countconnection == 0){
+                Connection connection = new Connection(" "," ", " ",process.getHost()," "," ", process.getPort());
+                result.getMissingServers().add(connection);
             }
         }
         return null;
